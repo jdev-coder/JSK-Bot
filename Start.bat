41e0bfe6246b859f4229b1c73a7b87406b21d193
jdev-coder <72287871+jdev-coder@users.noreply.github.com>
2020-10-13 19:43:21 -0700

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,18 +1,73 @@
 @ECHO OFF
 TITLE JSK Bot
 CHOICE /M "Do you really want to start the bot?"
+ 
 ECHO Bat Starting...
 TITLE JSK Bot, Starting...
+ 
 ECHO Scripts Starting...
 TITLE JSK Bot, Scripts Starting...
+ 
 ECHO Bot Starting...
 TITLE JSK Bot, Bot Starting...
+ 
 ECHO Done! Now loading...
 TITLE JSK Bot, Done! Loading now!
+ 
 TITLE JSK Bot, successfully loaded!
+ 
 TITLE JSK Bot
-
-cd C:\Users\Knight Mackay\Desktop\discordbot\Scripts
+ECHO Some art.                                            
+ECHO                                           :yyy.                                                    
+ECHO                              ``           +o+os                                                    
+ECHO                             sysyo.        :s++h`                                                   
+ECHO                             +ho++so.      :y++s+               .++-                                
+ECHO                         .:-  .ss+++s+.    /s+++h`              dsoyy:                              
+ECHO                      .+ysod/   :y++++oso+oo++++sy`             :ds++yy/                            
+ECHO                   `:syo+oh+`    -h++++++oo++++++ss`             `sh++oyyso/.                       
+ECHO                 -ohy++oho.       :h++++++++++++++os               +mo+++++osso/.`                  
+ECHO             `/oyyo+++sd-         `h+++++++++++++++s               .mo+oyhhysooossssso`             
+ECHO           `oyo+++++++hy          -h+++o++++++o+++so               :m+odddddddddhyso+sd:            
+ECHO         `/ho++ydddhy+sh          so+++/`yso+oo++yyy`              sy+dddmdddddddddddyod+           
+ECHO        :ys+oyddddmddh+m.         ss++++/syyos+++oooo             `d+sddhddhdddddddddddsho          
+ECHO      .syoshddddddmddmsys         `+ssso++++++++++++s+.           /h+ddhhhddhmhhhhdddddmyys`        
+ECHO     :hsohdddddddmhhhdmoy+          :dysoo+++++o+++++s/          `hoyddhhhhmhddhhhhhhdddmhyy`       
+ECHO    -dooddddddhhdmhhhhdh+yo.         hssysyo++oooo++ss`          /h+ddhhhhhddhmhhhhhhhdddddsy.      
+ECHO   `hy+dddddhhhmhmhhhhdddsoy/`       oyosyyssooooooy+`          `hoomdhhhhhhmhdhhhhhhhhdddddsh:     
+ECHO   od+hdddhhhhhmhmhhhhhhddhssy-      :hooosyssssooo/            /h+hdhhhhhhhddddhhhhhhhhhddddsh+`   
+ECHO  -moymddhhhhhhmhdhhhhhhhhhdyoh      -d+oooossyy``             -ho+ddhhhhhhhhmhmhhhhhhhhhhddddsyy-  
+ECHO `hyomdddhhhhhddhdhhhhhhhhhddoh-     :h+++oooosh             .oyooymdhhhhhhhhdhmhhhhhhhhhhhddddosd/ 
+ECHO /d+ddddhhhhhhddhhhhhhhhhhhhdhoy`    oy++++++++d-         `:ososy::ddhhhhhhhhhhmhhhhhhhhhhhhddddosd`
+ECHO `hssmddhhhhhhhdhhhhhhhhhhhhhdmoss`   ho++++++++sh`      -+ssoydmo :hmhhhhhhhhhhdhhhhhhhhhhhhhdddh+m:
+ECHO :moddddhhhhhhhddhhhhhhhhhhhhhdh+sy: -d++++++++++h+ .-:/ssso-+ddyooohdhhhhhhhhhhhhhdyoshhhhhhhhddmohs
+ECHO yyydddhhhhhd+osyhyyhmdhhhhhhhdmhsoyshs++++++++++ododsoooyds/-yo++++ssyddhhhhh+oshhd:  .:shhhhhhddyod
+ECHO mohddhhy+-++./s+os./ymhhhhhhhhddmhoyh+++++++++oyssssyysddddhooossos/-oydhyso:-:+yso`  .-.-ohdhhddh+m
+ECHO N+hdds:`  /ysooy+sooydys+:-:ohddyssd+++++++++++++++++ymddddmsooy++o+shdys/-----++o:+soo/`  .+hdddd+m
+ECHO mods-    -hooooosoo+yy/. `.-/sho+oho+ooooo++++++++++++sdddh-:d+oo+oymhho::------/o/o+-`      `/hdm+m
+ECHO dsd.     `s-:hhssso+++ssooooo++oshoo+/::/+sso+++++++++++sd/.:d+++y+-hhhhyys::/:-:+s-           `/mom
+ECHO ohy`      `---.://syso+++++ossshd+-.``````.-oyo++++osyso++osho++oh` :hyoohhosss+so`              yyd
+ECHO .`                ./osssss+:.+d:````````````:ys++++++osyoso++++h-   `  `syyohosd:               ./-
+ECHO                      `...   :d-``````````````.ys++++++++ydsoooh/         `` -h+oh-                 
+ECHO                            `h:````````````````.ys++++++++hdo+/.              ss+oh.                
+ECHO                            :h``````````````````.h+++++++++yo.                :d++oh`               
+ECHO                          `:ho```````````````````/y+++++++++oy+`              -m+++y+               
+ECHO                         :ssh+```````````````````.h+++++++++++sh-             +d++++h`              
+ECHO                       `oh++ys````````````````````os+++++++++++od/           .ds++++h-              
+ECHO                      `sh++++d.```````````````````:ho+++++++++++om/         -hy+++++h:              
+ECHO                      +m+++++ys.``````````````````:y+++++++++++++sm`      `+hs++++++d-              
+ECHO                      dh++++++ho.````````````````.h+++++++++++++++d+   `-+yy+++++++od`              
+ECHO                      od++++++oyy:.``````````````.d+++++++++++++++hdossyys+++++++++d/               
+ECHO                       oh++++++osho:...``````````.d+++++++++++++++hho++++++++++++odo                
+ECHO                        -syo++++oosys/:--.........ss++++++++++++++mo+++++++++++oym/                 
+ECHO                       .+yo+++++++osyhoo+::::-----:sy++++++++++++yy++++++++oosyhy-                  
+ECHO                   ./+sso++++++++++yy  `+dyoooo+++++shs+++++++++odooooooosssosy/                    
+ECHO                 .+/-.o+o++++++++++od`    :oso/::::::-:ysso++++++yyyyysso++ss:                      
+ECHO                 :/yo-``+o/o++osyso+.        -+osso/::/y+++++++++++ys:/oso/.                        
+ECHO                  :s//oy:.:ohys:`                `-:+om+ooo+++ooo++oom+.`                           
+ECHO                      :o/:.`.`                       .my: .s+s. :sy- .y`                            
+ECHO                                                      -+s: soy+. so++os/                            
+ECHO                                                        /y+  `+ss`  `/                             
+cd C:\Users\user\Desktop\discordbot\Scripts
 node main.js
-cd C:\Users\Knight Mackay\Desktop\discordbot\Discord-Auto-Publisher-master
+cd C:\Users\user\Desktop\discordbot\Discord-Auto-Publisher-master
 node bot.js
